9bf17f8c7d0718a80eb8af6daf6c6fac825d3a35
Roberto Fernandes <robertofernandes144@gmail.com>
2026-06-30 20:54:50 -0300

    fix: logger console-only for production

--- src/shared/utils/logger.ts
+++ src/shared/utils/logger.ts
@@ -10,18 +10,11 @@ export const logger = winston.createLogger({
   ),
   defaultMeta: { service: 'box-inteligente' },
   transports: [
-    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'logs/combined.log' }),
-  ],
-});
-
-if (env.NODE_ENV !== 'production') {
-  logger.add(
     new winston.transports.Console({
       format: winston.format.combine(
         winston.format.colorize(),
         winston.format.simple()
       ),
-    })
-  );
-}
+    }),
+  ],
+});
